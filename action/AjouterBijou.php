eb881deecab996c4dddb580bdbb6b35042fadb14
Marius <mariuspageot@gmail.com>
2019-03-11 16:33:17 +0100

    0.10

--- action/AjouterBijou.php
+++ action/AjouterBijou.php
@@ -5,3 +5,25 @@
  * Date: 11/03/2019
  * Time: 15:57
  */
+require_once '../Config.php';
+$nom= filter_input(INPUT_POST, "Nom");
+$prix=filter_input (INPUT_POST,"Prix");
+$premiere_tache=filter_input (INPUT_POST,"Premiere tache");
+$IDET=filter_input (INPUT_POST,"IDET");
+$temps=filter_input (INPUT_POST,"temps estime");
+$IDC=filter_input (INPUT_POST,"IDC");
+$db = new PDO("mysql:host=".Config::SERVERNAME.";dbname=".Config::DBNAME , Config::USER , Config::PASSWORD);
+$r = $db->prepare("insert into bijoux (nom,prix,premiere tache,IDET,Temps estime,IDC)"." values (:Nom, :prix, :premiere tache, :Temps estime,:IDC,:IDET)");
+
+$r->bindParam(":Nom",$nom);
+$r->bindParam(":prix",$prix);
+$r->bindParam(":premiere tache",$premiere_tache);
+$r->bindParam(":Temps estime",$temps);
+$r->bindParam(":IDC",$IDC);
+$r->bindParam(":IDET",$IDET);
+
+$r->execute();
+
+header("location: ../voir_categorie.php?id=$idcategorie");
+
+?>
--- action/AjouterEmploye.php
+++ action/AjouterEmploye.php
@@ -3,5 +3,28 @@
  * Created by PhpStorm.
  * User: Marius PAGEOT
  * Date: 11/03/2019
- * Time: 16:16
+ * Time: 15:57
  */
+require_once '../Config.php';
+$nom= filter_input(INPUT_POST, "Nom");
+$prenom=filter_input (INPUT_POST,"Prenom");
+$Grade=filter_input (INPUT_POST,"Grade");
+$IDM=filter_input (INPUT_POST,"IDM");
+$Status=filter_input (INPUT_POST,"Status");
+$Password=filter_input (INPUT_POST,"Password");
+$Login=filter_input (INPUT_POST,"Login");
+$db = new PDO("mysql:host=".Config::SERVERNAME.";dbname=".Config::DBNAME , Config::USER , Config::PASSWORD);
+$r = $db->prepare("insert into employees (Nom,Prenom,Grade,IDMetier,Statue,Password,Login)"." values (:Nom, :prenom, :Grade, :IDM, :Statue, :Password, :Login)");
+
+$r->bindParam(":Nom",$nom);
+$r->bindParam(":prenom",$prenom);
+$r->bindParam(":Grade",$Grade);
+$r->bindParam(":IDM",$IDM);
+$r->bindParam(":Status",$Status);
+$r->bindParam(":Password",$Password);
+$r->bindParam(":Login",$Login);
+
+$r->execute();
+
+
+?>
