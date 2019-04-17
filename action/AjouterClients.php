2d1b6857839e860954d402da4bb4913a09546686
joackim <joackim.29@hotmail.fr>
2019-04-17 10:56:49 +0200

    Nouveauclient ok

---
+++ action/AjouterClients.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joackim
+ * Date: 17/04/2019
+ * Time: 10:16
+ */
---
+++ vue/Liste-clients.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joackim
+ * Date: 16/04/2019
+ * Time: 12:00
+ */
